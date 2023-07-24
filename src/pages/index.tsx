a243cf5ecfb63acf80293ba88318cbc7fa8d4eba
Robin Kuiper <robingjkuiper@gmail.com>
2023-07-24 07:45:23 +0200

    Layout start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,179 +1,89 @@
+import { useState } from "react";
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import styled from "styled-components";
+import "../global.css";
+import archetypes from "../data/archetypes.json";
+import traits from "../data/traits.json";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-const headingAccentStyles = {
-  color: "#663399",
-};
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-};
-const doclistStyles = {
-  paddingLeft: 0,
-};
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-};
+const Container = styled.main`
+  display: flex;
+  flex-direction: row;
+`;
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-};
+const Navigation = styled.nav`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 200px;
+  box-sizing: border-box;
+  border-right: 1px solid #000;
+  //border: 1px solid red;
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: "inline-block",
-  marginBottom: 24,
-  marginRight: 12,
-};
+  a {
+    padding: 15px 10px;
+    border-bottom: 1px solid #000;
+  }
+`;
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-};
+const Content = styled.div``;
 
-const docLinks = [
+const categories = [
   {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
+    label: "Archetypes",
+    data: archetypes,
   },
   {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
+    label: "Traits",
+    data: traits,
   },
-];
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as const,
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-};
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description: "blaat",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description: "blaat2",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description: "Blaat 3",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description: "Blaat 4",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description: "dsfsdf",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description: "sdfs",
-    color: "#663399",
-  },
+  // "Weapons",
+  // "Mods",
+  // "Mutators",
+  // "Armor",
+  // "Relics",
+  // "Amulets",
+  // "Rings"
 ];
 
 const IndexPage: React.FC<PageProps> = () => {
+  const [category, setCategory] = useState(0);
+  const [data, setData] = useState(archetypes);
+
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
+    <Container>
+      <Navigation>
+        {categories.map((category, index) => (
+          <a
+            href="#"
+            key={category.label}
+            onClick={() => {
+              setCategory(index);
+              setData(categories[index].data);
+            }}
+          >
+            {category.label}
+          </a>
         ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-    </main>
+      </Navigation>
+
+      <Content>
+        <table>
+          <tr>
+            {Object.keys(data[0]).map(key => (
+              <th key={key}>{key}</th>
+            ))}
+          </tr>
+          {data.map((row, index) => (
+            <tr key={index}>
+              {Object.values(row).map(value => (
+                <td key={value}>{value}</td>
+              ))}
+            </tr>
+          ))}
+        </table>
+      </Content>
+    </Container>
   );
 };
 
