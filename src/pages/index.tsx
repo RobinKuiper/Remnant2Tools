9140952ad3e906bb8cfd89d7e5ea9cb310b7f50f
Robin Kuiper <robingjkuiper@gmail.com>
2023-07-25 07:53:45 +0200

    Styling / more data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,12 @@ import styled from "styled-components";
 import "../global.css";
 import archetypes from "../data/archetypes.json";
 import traits from "../data/traits.json";
+import weapons from "../data/weapons.json";
+import mods from "../data/mods.json";
+import mutators from "../data/mutators.json";
+import relics from "../data/relics.json";
+import amulets from "../data/amulets.json";
+import rings from "../data/rings.json";
 
 const Container = styled.main`
   display: flex;
@@ -17,6 +23,7 @@ const Navigation = styled.div`
   min-width: 200px;
   box-sizing: border-box;
   border-right: 1px solid #000;
+  background: #eeeded;
   //background: #fff;
   //border: 1px solid red;
 
@@ -34,6 +41,11 @@ const Navigation = styled.div`
 `;
 
 const Content = styled.div`
+  background: #fff;
+  //padding-left: 20px;
+  box-shadow: 0 0 30px 10px rgba(0, 0, 0, 0.8);
+  z-index: 1;
+
   table {
     table-layout: fixed;
     width: 100%;
@@ -44,10 +56,36 @@ const Content = styled.div`
       }
     }
 
-    tbody {
-      tr:nth-child(even) {
-        background: #ccc;
+    thead {
+      tr {
+        th {
+          padding: 10px 2px;
+          border-bottom: 2px solid #292929;
+        }
       }
+    }
+
+    tbody {
+      tr {
+        td {
+          padding: 15px 2px;
+
+          .redacted {
+            background-color: #000;
+            cursor: pointer;
+          }
+        }
+      }
+
+      tr td:first-child {
+        display: flex;
+        justify-content: center;
+      }
+
+      tr:nth-child(even) {
+        background: #eaeaea;
+      }
+
       tr:nth-child(odd) {
         background: #fff;
       }
@@ -64,13 +102,31 @@ const categories = [
     label: "Traits",
     data: traits,
   },
-  // "Weapons",
-  // "Mods",
-  // "Mutators",
+  {
+    label: "Weapons",
+    data: weapons,
+  },
+  {
+    label: "Mods",
+    data: mods,
+  },
+  {
+    label: "Mutators",
+    data: mutators,
+  },
   // "Armor",
-  // "Relics",
-  // "Amulets",
-  // "Rings"
+  {
+    label: "Relics",
+    data: relics,
+  },
+  {
+    label: "Amulets",
+    data: amulets,
+  },
+  {
+    label: "Rings",
+    data: rings,
+  },
 ];
 
 const IndexPage: React.FC<PageProps> = () => {
@@ -85,10 +141,14 @@ const IndexPage: React.FC<PageProps> = () => {
   }, []);
 
   const showValue = (key: string, value: any) => {
-    if (key === "name" || key === "description") {
+    if (key === "name" || key === "description" || key === "values" || key === "mod") {
       return value;
     } else {
-      return <span onClick={e => (e.currentTarget.innerHTML = value)}>SHOW</span>;
+      return (
+        <span className="redacted" onClick={e => e.currentTarget.classList.remove("redacted")}>
+          {value}
+        </span>
+      );
     }
   };
 
@@ -121,12 +181,17 @@ const IndexPage: React.FC<PageProps> = () => {
       </Navigation>
 
       <Content>
-        <table>
+        <table cellSpacing="0" cellPadding="0">
+          <col style={{ width: "40px" }} />
+          <col style={{ width: "200px" }} />
+          <col style={{ width: "50%" }} />
           <thead>
             <tr>
-              <th></th>
+              <th>
+                <input type="checkbox" />
+              </th>
               {Object.keys(categories[category].data[0]).map(key => (
-                <th key={key}>{key}</th>
+                <th key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}</th>
               ))}
             </tr>
           </thead>
@@ -134,7 +199,31 @@ const IndexPage: React.FC<PageProps> = () => {
             {categories[category].data.map((row, index) => (
               <tr key={index}>
                 <td>
-                  <input id={row.name} type="checkbox" checked={unlocks[row.name]} onChange={unlock} />
+                  {/*<input id={row.name} type="checkbox" checked={unlocks[row.name]} onChange={unlock} />*/}
+                  <div className="checkbox-wrapper-33">
+                    <label className="checkbox">
+                      <input
+                        id={row.name}
+                        className="checkbox__trigger visuallyhidden"
+                        type="checkbox"
+                        checked={unlocks[row.name]}
+                        onChange={unlock}
+                      />
+                      <span className="checkbox__symbol">
+                        <svg
+                          aria-hidden="true"
+                          className="icon-checkbox"
+                          width="28px"
+                          height="28px"
+                          viewBox="0 0 28 28"
+                          version="1"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M4 14l8 7L24 7"></path>
+                        </svg>
+                      </span>
+                    </label>
+                  </div>
                 </td>
                 {Object.entries(row).map(([key, value], index) => {
                   return <td key={value + index}>{showValue(key, value)}</td>;
