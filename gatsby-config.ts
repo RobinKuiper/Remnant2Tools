fc08e5d734b76aab557120ea2566a4d6fac8d8eb
Robin Kuiper <robingjkuiper@gmail.com>
2023-08-01 22:59:28 +0200

    Added htaccess

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: "Remnant",
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://remnant.rkuiper.nl",
   },
   // More easily incorporate content into your pages through
   // automatic TypeScript type generation and better GraphQL IntelliSense.
