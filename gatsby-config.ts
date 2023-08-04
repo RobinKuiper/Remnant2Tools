afd25f83bfd4e3e03f7ab8c272b08b4c6651ef94
Robin Kuiper <robingjkuiper@gmail.com>
2023-08-04 16:32:48 +0200

    Testing pages without pathprefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -39,7 +39,10 @@ const config: GatsbyConfig = {
       resolve: "gatsby-plugin-google-gtag",
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: ["G-8D16HJ20PQ"],
+        trackingIds: [
+          "G-8D16HJ20PQ", // Firebase
+          "G-5DDHSZZHS9", // Gitlab
+        ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
