b0e0c73bd3d3e4a0694ed5bc12f618ce173e6279
Robin Kuiper <robingjkuiper@gmail.com>
2023-08-07 09:40:47 +0200

    List layout improvements

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,10 +1,22 @@
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import {GatsbyImage, getImage} from "gatsby-plugin-image";
 import React from "react";
 import Redacted from "./Redacted";
 import ItemLevel from "./ItemLevel";
-import { Flex } from "../style/global";
-import { BsInfoCircleFill } from "react-icons/bs";
-import { Tooltip } from "react-tooltip";
+import {Flex} from "../style/global";
+import {BsInfoCircleFill} from "react-icons/bs";
+import {Tooltip} from "react-tooltip";
+import {styled} from "styled-components";
+
+const Container = styled.div`
+
+  .title {
+    text-align: left;
+  }
+
+  .field {
+    text-align: right;
+  }
+`
 
 interface Props {
   item: any;
@@ -18,23 +30,24 @@ interface Props {
 }
 
 const ListItem = (props: Props) => {
-  const { item, category, type = "tracker", image, unlocked, handleChange, level, setLevel } = props;
+  const {item, category, type = "tracker", image, unlocked, handleChange, level, setLevel} = props;
 
   return (
-    <Flex direction="row" justifyContent="space-between" alignItems="center">
-      <Flex alignItems="center">
-        <Flex justifyContent="center">
-          {type === "tracker" && (
-            <div className="checkbox-wrapper">
-              <label className="checkbox">
-                <input
-                  id={item.id}
-                  className="checkbox__trigger visuallyhidden"
-                  type="checkbox"
-                  checked={unlocked}
-                  onChange={handleChange}
-                />
-                <span className="checkbox__symbol">
+    <Container>
+      <Flex direction="row" justifyContent="space-between" alignItems="center">
+        <Flex alignItems="center">
+          <Flex justifyContent="center">
+            {type === "tracker" && (
+              <div className="checkbox-wrapper">
+                <label className="checkbox">
+                  <input
+                    id={item.id}
+                    className="checkbox__trigger visuallyhidden"
+                    type="checkbox"
+                    checked={unlocked}
+                    onChange={handleChange}
+                  />
+                  <span className="checkbox__symbol">
                   <svg
                     aria-hidden="true"
                     className="icon-checkbox"
@@ -47,72 +60,75 @@ const ListItem = (props: Props) => {
                     <path d="M4 14l8 7L24 7"></path>
                   </svg>
                 </span>
-              </label>
-            </div>
+                </label>
+              </div>
+            )}
+
+            {type === "tracker" && category.hasLevels && <ItemLevel level={level} setLevel={setLevel}/>}
+          </Flex>
+
+          {image && (
+            <GatsbyImage
+              image={getImage(image)}
+              alt={item.name}
+              title={item.name}
+              placeholder="none"
+              style={
+                image.childImageSharp.gatsbyImageData.height > image.childImageSharp.gatsbyImageData.width
+                  ? {height: "100px"}
+                  : {width: "100px"}
+              }
+            />
           )}
 
-          {type === "tracker" && category.hasLevels && <ItemLevel level={level} setLevel={setLevel} />}
+          <div className="title">
+            <h3>{item.name}</h3>
+
+            {item.description && <p>{item.description}</p>}
+          </div>
         </Flex>
 
-        {image && (
-          <GatsbyImage
-            image={getImage(image)}
-            alt={item.name}
-            title={item.name}
-            placeholder="none"
-            style={
-              image.childImageSharp.gatsbyImageData.height > image.childImageSharp.gatsbyImageData.width
-                ? { height: "100px" }
-                : { width: "100px" }
-            }
-          />
-        )}
+        <Flex alignItems="center" justifyContent="right" gap="40px">
+          {category &&
+            category[type].fields.map(field => {
 
-        <div className="title">
-          <h3>{item.name}</h3>
+              if (item[field.fragment] && item[field.fragment] !== "") {
+                return (
+                  <div key={field.fragment} className="field">
+                    <Flex direction="column">
+                      <div className="field-title">{field.label}</div>
+                      <div>
+                        {field.redacted && !unlocked ? (
+                          <Redacted value={item[field.fragment]} defaultShow={unlocked} bgColor={"#c7c7c7"}/>
+                        ) : (
+                          item[field.fragment]
+                        )}
+                      </div>
+                    </Flex>
+                  </div>
+                )
+              }
 
-          {item.description && <p>{item.description}</p>}
-        </div>
-      </Flex>
+            })}
+        </Flex>
 
-      <Flex alignItems="center" justifyContent="space-around" gap="40px">
-        {category &&
-          category[type].fields.map(field => (
-            <div key={field.fragment}>
-              {item[field.fragment] && item[field.fragment] !== "" && (
-                <Flex direction="row">
-                  <Flex direction="column">
-                    <div className="field-title">{field.label}</div>
-                    <div>
-                      {field.redacted && !unlocked ? (
-                        <Redacted value={item[field.fragment]} defaultShow={unlocked} bgColor={"#c7c7c7"} />
-                      ) : (
-                        item[field.fragment]
-                      )}
-                    </div>
-                  </Flex>
-                </Flex>
-              )}
+        {item.unlock && (
+          <>
+            <div className="unlock-information">
+              <button data-tooltip-id={`${item.name}_tooltip`} data-tooltip-content={item.unlock}>
+                <BsInfoCircleFill/> Unlock information
+              </button>
             </div>
-          ))}
+            <Tooltip
+              id={`${item.name}_tooltip`}
+              style={{
+                maxWidth: "200px",
+              }}
+            />
+          </>
+        )}
       </Flex>
-
-      {item.unlock && (
-        <>
-          <div className="unlock-information">
-            <button data-tooltip-id={`${item.name}_tooltip`} data-tooltip-content={item.unlock}>
-              <BsInfoCircleFill /> Unlock information
-            </button>
-          </div>
-          <Tooltip
-            id={`${item.name}_tooltip`}
-            style={{
-              maxWidth: "200px",
-            }}
-          />
-        </>
-      )}
-    </Flex>
+    </Container>
   );
 };
 
