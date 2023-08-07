c1e2c0c3a8cbb76d32d9fbe7b84f1bad5c662a61
Robin Kuiper <robingjkuiper@gmail.com>
2023-08-07 22:28:18 +0200

    Fixed pages

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   transform: {
-    "^.+\\.[jt]sx?$": "<rootDir>/jest-preprocess.cjs",
+    "^.+\\.[jt]sx?$": "<rootDir>/jest-preprocess.js",
   },
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.cjs`,
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
   },
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
   transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link)/)`],
@@ -16,6 +16,6 @@ module.exports = {
   testEnvironmentOptions: {
     url: `http://localhost:8000`,
   },
-  setupFiles: [`<rootDir>/loadershim.cjs`],
+  setupFiles: [`<rootDir>/loadershim.js`],
   coverageReporters: ["clover", "json", "text", "lcov", "cobertura"],
 };
