87da5aea0f30594112a80289bdf1e8f24d14a749
Robin Kuiper <robingjkuiper@gmail.com>
2023-08-11 14:04:08 +0200

    Test gitlab

---
+++ scripts/updateGitlab.py
@@ -0,0 +1,94 @@
+import os
+import subprocess
+import gitlab
+
+# Extract version from .version file
+with open(".version", "r") as version_file:
+    CURRENT_VERSION = version_file.read().strip()
+
+# Extract major and minor version components
+MAJOR_VERSION_NUMBERS = ".".join(CURRENT_VERSION.split(".")[:2])
+
+# Calculate the next patch version
+NEXT_VERSION = str(int(CURRENT_VERSION.split(".")[-1]) + 1)
+
+# Create release and milestone names
+RELEASE_NAME = f"Release v{CURRENT_VERSION}"
+MILESTONE_NAME = f"Release v{MAJOR_VERSION_NUMBERS}.{NEXT_VERSION}"
+
+# Extract latest release section from UPDATES.md
+with open("UPDATES.md", "r") as updates_file:
+    updates_lines = updates_file.readlines()
+
+section_lines = []
+found_section = False
+for line in updates_lines:
+    if line.startswith("## Version"):
+        if found_section:
+            break
+        found_section = True
+    if found_section:
+        section_lines.append(line)
+
+UPDATES_TEXT = "\n".join(section_lines)
+
+# Define issue title and description
+READY_RELEASE_ISSUE_TITLE = "Ready release"
+READY_RELEASE_ISSUE_DESCRIPTION = (
+    "- [ ] Update version in .version\n- [ ] Add/update update log to UPDATES.md"
+)
+DEPENDENCY_ISSUE_TITLE = "Check & update dependencies"
+RELEASE_ISSUE_TITLE = MILESTONE_NAME
+
+# Get the environment variables from Gitlab
+GITLAB_TOKEN = os.environ.get("CI_JOB_TOKEN")
+PROJECT_ID = os.environ.get("CI_PROJECT_ID")
+REF_NAME = os.environ.get("CI_COMMIT_REF_NAME")
+
+gl = gitlab.Gitlab("https://gitlab.com", job_token=GITLAB_TOKEN)
+
+gl.enable_debug()
+
+# Get the project from gitlab
+project = project = gl.projects.get(PROJECT_ID, lazy=True)
+
+# Create a new release
+release = project.releases.create(
+    {
+        "name": RELEASE_NAME,
+        "tag_name": CURRENT_VERSION,
+        "description": UPDATES_TEXT,
+        "ref": REF_NAME,
+    }
+)
+
+# Create a new milestone
+milestone = project.milestones.create({"title": MILESTONE_NAME})
+
+# Create the ready release issue
+issue = project.issues.create(
+    {
+        "title": READY_RELEASE_ISSUE_TITLE,
+        "description": READY_RELEASE_ISSUE_DESCRIPTION,
+        "milestone_id": milestone.id,
+    }
+)
+
+issue.labels = ["Release", "Final Touches"]
+issue.save()
+
+# Create the dependencies issue
+issue = project.issues.create(
+    {"title": DEPENDENCY_ISSUE_TITLE, "description": "", "milestone_id": milestone.id}
+)
+
+issue.labels = ["Improvement", "Final Touches"]
+issue.save()
+
+# Create the release issue
+issue = project.issues.create(
+    {"title": RELEASE_ISSUE_TITLE, "description": "", "milestone_id": milestone.id}
+)
+
+issue.labels = ["Release", "Final Touches"]
+issue.save()
