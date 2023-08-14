1cb86f7b09524344e4cf2ee4064f9a0deeac2f91
Robin Kuiper <robingjkuiper@gmail.com>
2023-08-14 18:30:27 +0200

    ci: Stop building on release

--- scripts/ignoreBuild.sh
+++ scripts/ignoreBuild.sh
@@ -2,7 +2,8 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "development" || "$VERCEL_GIT_COMMIT_REF" == "master" || "$VERCEL_GIT_COMMIT_REF" == release/* ]]; then
+# || "$VERCEL_GIT_COMMIT_REF" == release/*
+if [[ "$VERCEL_GIT_COMMIT_REF" == "development" || "$VERCEL_GIT_COMMIT_REF" == "master" ]]; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1
