520ecaa18851d0c7809b3a7177ebde030207c8ba
Robin Kuiper <robingjkuiper@gmail.com>
2023-08-14 23:34:26 +0200

    ci: Also include time in release date

--- scripts/preBuilt.sh
+++ scripts/preBuilt.sh
@@ -9,5 +9,5 @@ VERSION=$(cat .version)
 # Replace placeholders with the version and current date in src/constants.ts and static/humans.txt
 sed -i "s/SITE_VERSION/$VERSION/g" src/constants.ts
 sed -i "s/SITE_VERSION/$VERSION/g" static/humans.txt
-sed -i "s/CURRENT_DATE/$(date '+%Y-%m-%d')/g" src/constants.ts
-sed -i "s/CURRENT_DATE/$(date '+%Y-%m-%d')/g" static/humans.txt
+sed -i "s/CURRENT_DATE/$(date '+%Y-%m-%d %H:%M:%S')/g" src/constants.ts
+sed -i "s/CURRENT_DATE/$(date '+%Y-%m-%d %H:%M:%S')/g" static/humans.txt
