2756021fe6255674bc23c56a6589c6ea912ee40c
Robin Kuiper <robingjkuiper@gmail.com>
2023-08-16 12:32:10 +0200

    style(Breadcrumb): Improved breadcrumb style

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -63,7 +63,7 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`Fontdiner Swanky`, `roboto`],
+        fonts: [`Fontdiner Swanky`, `roboto`, `Bricolage Grotesque`, `Montserrat`],
         display: "swap",
       },
     },
