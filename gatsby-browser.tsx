264eefaf2eea89eba0aa913358cf3f8ffc37f76c
Robin Kuiper <robingjkuiper@gmail.com>
2023-08-16 16:42:58 +0200

    chore(Vercel): Added Vercel analytics

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Wrapper from "./src/components/Wrapper";
+import {Analytics} from "@vercel/analytics/dist/react";
 
 export const wrapRootElement = ({ element }) => {
-  return <Wrapper>{element}</Wrapper>;
+  return (
+    <>
+      <Wrapper>{element}</Wrapper>
+      <Analytics />
+    </>
+  );
 };
