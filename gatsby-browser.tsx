e93ecee6eeda03a2b7fdc8a2cc3f74137abb5f76
Robin Kuiper <robingjkuiper@gmail.com>
2023-08-16 16:53:41 +0200

    chore(Vercel): Fix Vercel analytics

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Wrapper from "./src/components/Wrapper";
-import {Analytics} from "@vercel/analytics/dist/react";
+import {Analytics} from "@vercel/analytics/react";
 
 export const wrapRootElement = ({ element }) => {
   return (
