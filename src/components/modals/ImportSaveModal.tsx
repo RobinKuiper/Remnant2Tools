9d16748615b4e7220ccab3fc4295f5e71a8d1dcc
Robin Kuiper <robingjkuiper@gmail.com>
2023-08-23 07:35:44 +0200

    feat(SaveAnalyzer): Convert save on the server

--- src/components/modals/ImportSaveModal.tsx
+++ src/components/modals/ImportSaveModal.tsx
@@ -1,12 +1,14 @@
-import React, { useContext, useState } from "react";
+import React, {useState} from "react";
 import Modal from "react-modal";
-import { styled } from "styled-components";
-import { Link, graphql, useStaticQuery } from "gatsby";
+import {styled} from "styled-components";
+import {Link, graphql, useStaticQuery} from "gatsby";
 import Loader from "../Loader";
-import { DataContext } from "../../context/DataContext";
-import { FileUploader } from "react-drag-drop-files";
-import { toast } from "react-toastify";
-import { AiOutlineFileAdd, AiOutlineMergeCells, AiOutlineWarning } from "react-icons/ai";
+import {FileUploader} from "react-drag-drop-files";
+import {toast} from "react-toastify";
+import {AiOutlineFileAdd, AiOutlineMergeCells, AiOutlineWarning} from "react-icons/ai";
+import {useAppDispatch, useAppSelector} from "../../hooks";
+import {RootState} from "../../store";
+import {updateUnlocks} from "../../features/data/dataSlice";
 
 Modal.setAppElement("#___gatsby");
 
@@ -15,7 +17,7 @@ interface Props {
   isOpen: boolean;
 }
 
-const ImportSaveModal = ({ setIsOpen, isOpen }: Props) => {
+const ImportSaveModal = ({setIsOpen, isOpen}: Props) => {
   const data = useStaticQuery(graphql`
     {
       items: allItem(
@@ -46,19 +48,20 @@ const ImportSaveModal = ({ setIsOpen, isOpen }: Props) => {
       }
     }
   `);
-  const { items } = data;
-  const { unlocks, updateUnlocks } = useContext(DataContext);
+  const {items} = data;
+  const dispatch = useAppDispatch();
+  const { unlocks } = useAppSelector((state: RootState) => state.data);
   const [loading, setLoading] = useState(false);
   // const [notInUnlocksData, setNotInUnlocksData] = useState<{ name: string; category: string }[] | null>();
   const [notInUnlocksIds, setNotInUnlocksIds] = useState<number[] | null>();
 
   const closeModal = () => {
     setIsOpen(false);
-    setNotInUnlocksData(null);
+    // setNotInUnlocksData(null);
     setNotInUnlocksIds(null);
   };
 
-  const handleFile = files => {
+  const handleFile = async files => {
     const file = files[0];
     if (!file) {
       return;
@@ -66,50 +69,56 @@ const ImportSaveModal = ({ setIsOpen, isOpen }: Props) => {
 
     setLoading(true);
 
-    const fr = new FileReader();
-    fr.onload = e => {
-      const contents = e.target.result;
+    const formdata = new FormData();
+    formdata.append("file", file);
+    const result = await fetch("/api/data/convert", {
+      method: "POST",
+      body: formdata,
+    });
 
-      const decompressedData = contents.toString().toLowerCase();
-      const foundIds = [];
-      items.nodes.forEach(item => {
-        const name = item.name.replace(/[^a-zA-Z]/g, "").toLowerCase();
-        // console.log(`Checking ${name}`)
-        if (decompressedData.includes(name)) {
-          // console.log(`File includes ${name}`)
-          foundIds.push(item.externalId);
-        }
-      });
+    const {converted: contents} = await result.json();
 
-      // const notInCurrentUnlocks = [];
-      const idsNotInCurrentUnlocks = [];
-      foundIds.forEach(id => {
-        const item = items.nodes.find(item => item.externalId === id);
-        if (!unlocks.includes(id) && item) {
-          // notInCurrentUnlocks.push({ name: item.name, category: item.category });
-          idsNotInCurrentUnlocks.push(id);
-        }
-      });
+    const decompressedData = contents.toString().toLowerCase();
+    const foundIds = [];
+    items.nodes.forEach(item => {
+      const name = item.name.replace(/[^a-zA-Z]/g, "").toLowerCase();
+      // console.log(`Checking ${name}`)
+      if (decompressedData.includes(name)) {
+        // console.log(`File includes ${name}`)
+        foundIds.push(item.externalId);
+      }
+    });
 
-      // setNotInUnlocksData(notInCurrentUnlocks);
-      setNotInUnlocksIds(idsNotInCurrentUnlocks);
-      setLoading(false);
-    };
-    fr.readAsText(file);
+    // const notInCurrentUnlocks = [];
+    const idsNotInCurrentUnlocks = [];
+    foundIds.forEach(id => {
+      const item = items.nodes.find(item => item.externalId === id);
+      if (!unlocks.includes(id) && item) {
+        // notInCurrentUnlocks.push({ name: item.name, category: item.category });
+        idsNotInCurrentUnlocks.push(id);
+      }
+    });
+
+    // setNotInUnlocksData(notInCurrentUnlocks);
+    setNotInUnlocksIds(idsNotInCurrentUnlocks);
+    setLoading(false);
   };
+  
+  const endConvert = () => {
+    dispatch(updateUnlocks())
+    closeModal();
+  }
 
   const mergeData = () => {
     localStorage.setItem("data", JSON.stringify([...unlocks, ...notInUnlocksIds]));
-    updateUnlocks();
     toast.success("Successfully merged data");
-    closeModal();
+    endConvert();    
   };
 
   const overwriteData = () => {
     localStorage.setItem("data", JSON.stringify(notInUnlocksIds));
-    updateUnlocks();
     toast.success("Successfully overwritten data");
-    closeModal();
+    endConvert();
   };
 
   const renderFileUploaderOrResult = () => {
@@ -119,17 +128,17 @@ const ImportSaveModal = ({ setIsOpen, isOpen }: Props) => {
           multiple={true}
           handleChange={handleFile}
           name="file"
-          types={["txt"]}
+          types={["sav"]}
           classes="dropzone"
           children={
             <div className="dropzone-content">
-              <AiOutlineFileAdd size={25} color="#a65252" />
+              <AiOutlineFileAdd size={25} color="#a65252"/>
               <div className="dropzone-description">
                 <span>
                   <span className="underline">Upload</span> or drop a file right here
                 </span>
-                <span title="types: txt" className="file-types">
-                  txt
+                <span title="types: sav" className="file-types">
+                  sav
                 </span>
               </div>
             </div>
@@ -144,11 +153,11 @@ const ImportSaveModal = ({ setIsOpen, isOpen }: Props) => {
           </p>
           <div className="buttons">
             <button className="merge" onClick={mergeData}>
-              <AiOutlineMergeCells size={25} />
+              <AiOutlineMergeCells size={25}/>
               Merge
             </button>
             <button className="overwrite" onClick={overwriteData}>
-              <AiOutlineWarning size={25} />
+              <AiOutlineWarning size={25}/>
               Overwrite
             </button>
           </div>
@@ -178,7 +187,7 @@ const ImportSaveModal = ({ setIsOpen, isOpen }: Props) => {
             <Link to="https://github.com/Razzmatazzz/RemnantSaveGuardian/releases" _target="_blank">
               Remnant Save Guardian
             </Link>
-            <br />
+            <br/>
             In{" "}
             <Link to="https://github.com/Razzmatazzz/RemnantSaveGuardian/releases" _target="_blank">
               Remnant Save Guardian
@@ -195,7 +204,7 @@ const ImportSaveModal = ({ setIsOpen, isOpen }: Props) => {
           </p>
         </div>
 
-        {loading ? <Loader loading={loading} color="#000" /> : renderFileUploaderOrResult()}
+        {loading ? <Loader loading={loading} color="#000"/> : renderFileUploaderOrResult()}
       </Content>
     </Modal>
   );
